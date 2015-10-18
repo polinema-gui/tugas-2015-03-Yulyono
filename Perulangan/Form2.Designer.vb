28ca44bc2e3a1f8a79148ddb41278b081ba04c74
Yulyono <yulyono.17@gmail.com>
2015-10-18 21:38:19 +0700

    Tugas 3

--- Perulangan/Form2.Designer.vb
+++ Perulangan/Form2.Designer.vb
@@ -22,19 +22,89 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.hasilBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.barisString = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Pola = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(368, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 19)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'hasilBox
+        '
+        Me.hasilBox.Location = New System.Drawing.Point(17, 112)
+        Me.hasilBox.Multiline = True
+        Me.hasilBox.Name = "hasilBox"
+        Me.hasilBox.Size = New System.Drawing.Size(432, 275)
+        Me.hasilBox.TabIndex = 16
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Baris"
+        '
+        'barisString
+        '
+        Me.barisString.Location = New System.Drawing.Point(59, 75)
+        Me.barisString.Name = "barisString"
+        Me.barisString.Size = New System.Drawing.Size(288, 20)
+        Me.barisString.TabIndex = 14
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Pola"
+        '
+        'Pola
+        '
+        Me.Pola.FormattingEnabled = True
+        Me.Pola.Items.AddRange(New Object() {"Piramyd", "Hollow Piramyd", "Inverted Piramyd", "Hollow Inverted Piramyd"})
+        Me.Pola.Location = New System.Drawing.Point(59, 37)
+        Me.Pola.Name = "Pola"
+        Me.Pola.Size = New System.Drawing.Size(391, 21)
+        Me.Pola.TabIndex = 12
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(465, 425)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.hasilBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.barisString)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Pola)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form2"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Problem Bintang"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents hasilBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents barisString As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Pola As System.Windows.Forms.ComboBox
 End Class
--- Perulangan/Form3.Designer.vb
+++ Perulangan/Form3.Designer.vb
@@ -22,19 +22,134 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btHitung = New System.Windows.Forms.Button()
+        Me.tbMatriks2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.tbMatriks1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'btHitung
+        '
+        Me.btHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btHitung.Location = New System.Drawing.Point(368, 353)
+        Me.btHitung.Name = "btHitung"
+        Me.btHitung.Size = New System.Drawing.Size(67, 26)
+        Me.btHitung.TabIndex = 36
+        Me.btHitung.Text = "Hitung"
+        Me.btHitung.UseVisualStyleBackColor = True
+        '
+        'tbMatriks2
+        '
+        Me.tbMatriks2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbMatriks2.Location = New System.Drawing.Point(73, 168)
+        Me.tbMatriks2.Multiline = True
+        Me.tbMatriks2.Name = "tbMatriks2"
+        Me.tbMatriks2.Size = New System.Drawing.Size(362, 80)
+        Me.tbMatriks2.TabIndex = 35
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox3.Location = New System.Drawing.Point(73, 255)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(362, 92)
+        Me.TextBox3.TabIndex = 34
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Tambah", "Kurang"})
+        Me.ComboBox1.Location = New System.Drawing.Point(73, 126)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(362, 21)
+        Me.ComboBox1.TabIndex = 33
+        '
+        'tbMatriks1
+        '
+        Me.tbMatriks1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbMatriks1.Location = New System.Drawing.Point(73, 26)
+        Me.tbMatriks1.Multiline = True
+        Me.tbMatriks1.Name = "tbMatriks1"
+        Me.tbMatriks1.Size = New System.Drawing.Size(362, 83)
+        Me.tbMatriks1.TabIndex = 32
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(7, 255)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 15)
+        Me.Label4.TabIndex = 31
+        Me.Label4.Text = "Hasil"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 15)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Matriks 2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 15)
+        Me.Label2.TabIndex = 29
+        Me.Label2.Text = "Operasi"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 15)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "Matriks 1"
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(441, 402)
+        Me.Controls.Add(Me.btHitung)
+        Me.Controls.Add(Me.tbMatriks2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.tbMatriks1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form3"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Matriks Kalkulator"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btHitung As System.Windows.Forms.Button
+    Friend WithEvents tbMatriks2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents tbMatriks1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
